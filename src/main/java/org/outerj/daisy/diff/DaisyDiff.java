e76f6ff5e42339bbadc82ab8726a1b8f12dbc791
sshanahin <sshanahin@modeln.com>
2018-02-22 00:47:54 +0200

    added ability of simple HTML diff with default css, clean up of pom.xml, added sources to packaging, changed version

--- src/main/java/org/outerj/daisy/diff/DaisyDiff.java
+++ src/main/java/org/outerj/daisy/diff/DaisyDiff.java
@@ -16,6 +16,7 @@
 package org.outerj.daisy.diff;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.util.Locale;
 
@@ -30,7 +31,15 @@ import org.xml.sax.ContentHandler;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.AttributesImpl;
 import org.xml.sax.helpers.XMLReaderFactory;
+
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXTransformerFactory;
+import javax.xml.transform.sax.TransformerHandler;
+import javax.xml.transform.stream.StreamResult;
 
 public class DaisyDiff {
 
@@ -59,6 +68,63 @@ public class DaisyDiff {
         HtmlSaxDiffOutput output = new HtmlSaxDiffOutput(consumer, prefix);
         HTMLDiffer differ = new HTMLDiffer(output);
         differ.diff(leftComparator, rightComparator);
+    }
+
+    /**
+     *  Diffs two HTML files, outputting a diff to the provided output File as HTML with default styles imported
+     * @param oldSource first files to compare
+     * @param newSource second file to compare
+     * @param outputFile a File to which output will be written
+     * @param xslPath path to XSL for transforming diff to HTML (header and styles) if null or empty -- generates simple HTML diff with default styles
+     * @throws TransformerConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     */
+    public static void diffHtmlSimple(InputSource oldSource, InputSource newSource, File outputFile, String xslPath) throws TransformerConfigurationException, IOException, SAXException {
+        final SAXTransformerFactory tf = (SAXTransformerFactory) TransformerFactory.newInstance();
+
+        final TransformerHandler res = tf.newTransformerHandler();
+        res.getTransformer().setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        res.getTransformer().setOutputProperty(OutputKeys.INDENT, "yes");
+        res.getTransformer().setOutputProperty(OutputKeys.METHOD, "html");
+        res.getTransformer().setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+        res.setResult(new StreamResult(outputFile));
+
+        final XslFilter filter = new XslFilter();
+
+        if (xslPath == null || xslPath.trim().isEmpty()) {
+            xslPath = "xslfilter/simplehtmlwithcss.xsl";
+        }
+
+        ContentHandler postProcess = filter.xsl(res, xslPath);
+
+        Locale locale = Locale.getDefault();
+        String prefix = "diff";
+
+        HtmlCleaner cleaner = new HtmlCleaner();
+
+        DomTreeBuilder oldHandler = new DomTreeBuilder();
+        cleaner.cleanAndParse(oldSource, oldHandler);
+        TextNodeComparator leftComparator = new TextNodeComparator(oldHandler, locale);
+
+        DomTreeBuilder newHandler = new DomTreeBuilder();
+        cleaner.cleanAndParse(newSource, newHandler);
+        TextNodeComparator rightComparator = new TextNodeComparator(newHandler, locale);
+
+        postProcess.startDocument();
+
+        postProcess.startElement("", "diffreport", "diffreport", new AttributesImpl());
+        postProcess.startElement("", "diff", "diff", new AttributesImpl());
+
+        HtmlSaxDiffOutput output = new HtmlSaxDiffOutput(postProcess, prefix);
+        HTMLDiffer differ = new HTMLDiffer(output);
+
+        differ.diff(leftComparator, rightComparator);
+
+        postProcess.endElement("", "diff", "diff");
+        postProcess.endElement("", "diffreport", "diffreport");
+
+        postProcess.endDocument();
     }
 
     /**
